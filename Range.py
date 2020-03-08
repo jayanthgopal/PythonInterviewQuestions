95a14fc90e32c2030e260033afca3fba9ec06313
Jayanth <jaijayanth@gmail.com>
2020-03-08 16:04:21 +0000

    Questions

--- Range.py
+++ Range.py
@@ -1,3 +1,9 @@
+"""
+Print the difference between the highest and the lowest number
+in the list.
+"""
+
+
 def get_range(nums):
     sort_list = list(sorted(nums))
     return sort_list[-1] - sort_list[0]
--- fib.py
+++ fib.py
@@ -1,11 +1,16 @@
 # (0),1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+"""
+Print nth fibonacci number
+"""
+
+
 def fibonacci(n):
     if n == 1:
         return 1
     if n == 0:
         return 0
-    return fibonacci(n-1) + fibonacci(n-2)
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 
 print(fibonacci(9))
-
